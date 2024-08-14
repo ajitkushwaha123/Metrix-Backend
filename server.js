e5b13b11f474998b2aea7e1db41bef9857c6bd65
ajitkushwaha123 <ajitkushwahaswe@gmail.com>
2024-08-14 08:41:12 +0530

    Port Update

--- server.js
+++ server.js
@@ -6,9 +6,8 @@ import router from "./router/route.js";
 import ENV from "./config.js";
 
 const app = express();
-const port = ENV.PORT;
+const port = ENV.PORT || 5000;
 
-// Middleware
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(
@@ -17,29 +16,26 @@ app.use(
     methods: "GET,POST,PUT,DELETE,OPTIONS",
     allowedHeaders:
       "Origin, X-Requested-With, Content-Type, Accept, Authorization",
-    credentials: true, // Allow credentials
+    credentials: true, 
     preflightContinue: false,
     optionsSuccessStatus: 204,
   })
 );
-app.options("*", cors()); // Handle preflight requests
+app.options("*", cors());
 app.use(morgan("tiny"));
 app.disable("x-powered-by");
 
-// Routes
 app.get("/", (req, res) => {
   res.status(201).json("Home GET Request");
 });
 
 app.use("/api", router);
 
-// Global error handler
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).json({ error: "Something went wrong!" });
 });
 
-// Database connection and server start
 connect()
   .then(() => {
     app.listen(port, () => {
