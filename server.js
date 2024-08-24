6adb19958e6e3de79e2c67ea3a4a4664f866b931
ajitkushwaha123 <ajitkushwahaswe@gmail.com>
2024-08-24 18:16:47 +0530

    update server.js

--- server.js
+++ server.js
@@ -14,7 +14,11 @@ app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://crm.magicscale.in"], // Add your frontend domain here
+    origin: [
+      "http://localhost:5173",
+      "https://crm.magicscale.in",
+      "http://82.112.237.229",
+    ], // Add your frontend domain here
     methods: "GET,POST,PUT,DELETE,OPTIONS",
     allowedHeaders:
       "Origin, X-Requested-With, Content-Type, Accept, Authorization",
