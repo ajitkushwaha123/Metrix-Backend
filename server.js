ffa87d84733340c34e74a4f550f7b02c5861fc47
ajitkushwaha123 <ajitkushwahaswe@gmail.com>
2024-08-25 11:10:45 +0530

    update Server.js

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const port = process.env.PORT || 5000;
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
+
 app.use(
   cors({
     origin: [
@@ -27,6 +28,7 @@ app.use(
     optionsSuccessStatus: 204,
   })
 );
+
 app.options("*", cors());
 app.use(morgan("tiny"));
 app.disable("x-powered-by");
